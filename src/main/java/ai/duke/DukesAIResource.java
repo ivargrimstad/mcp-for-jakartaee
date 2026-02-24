5c8fc5ce55617470af039b971c6ca730d3de9989
Ivar Grimstad <ivar.grimstad@eclipse-foundation.org>
2026-02-24 11:46:35 +0100

    Add initial structure

---
+++ src/main/java/ai/duke/DukesAIResource.java
@@ -0,0 +1,29 @@
+package ai.duke;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
+import org.omnifaces.ai.AIProvider;
+import org.omnifaces.ai.AIService;
+import org.omnifaces.ai.cdi.AI;
+
+@Path("ai")
+public class DukesAIResource {
+
+    @Inject
+    @AI(provider = AIProvider.OPENAI,
+            apiKey = "${System.getenv('OPENAI_API_KEY')}",
+            model = "gpt-5.2"
+    )
+    private AIService aiService;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String callAI(@QueryParam("message") String message) {
+
+        return aiService.chat(message);
+    }
+}
